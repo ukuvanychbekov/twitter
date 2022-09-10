c7f92a994bc3a623b7ee9905948987b696969ca4
ukuvanychbekov <u.kuvanychbekov@gmail.com>
2022-09-10 19:21:42 +0600

    first

---
+++ post/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+
+from .models import Tweet, Comment
+
+
+class TweetSerializer(serializers.ModelSerializer):
+    post_username = serializers.ReadOnlyField()
+
+    class Meta:
+        model = Tweet
+        fields = "__all__"
+        read_only_fields = ['user',]
+
+class CommentSerializer(serializers.ModelSerializer):
+    post_username = serializers.ReadOnlyField()
+
+    class Meta:
+        model = Comment
+        fields = "__all__"
+        read_only_fields = ['user',]
